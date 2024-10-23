32240ae78ecf2395a2b714ddecdc8dff18f5c6ea
Adam Detmer <adam19092201@gmail.com>
2024-10-23 22:25:00 +0200

    modyfikowana zmiana z rebase

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,8 +1,12 @@
+// Pierwszy komentarz
 public class Main {
+    int y = 5;
     int x = 10;
 
     public static void main(String[] args) {
+        System.out.println("COS");
         Main myObj = new Main();
         System.out.println(myObj.x);
+        int z = 3;
     }
 }
